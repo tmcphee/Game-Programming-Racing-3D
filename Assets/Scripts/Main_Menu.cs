bf32640320fa6f2a4e73a54a9ca76713d1088b34
Troy Walther <tdgames29@gmail.com>
2020-03-30 16:37:14 -0400

    Added movement, And menu

---
+++ Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Main_Menu : MonoBehaviour {
+
+	public void playGame(){
+		SceneManager.LoadScene("Multiplayer", LoadSceneMode.Single);
+	}
+
+	public void quitGame(){
+		Debug.Log ("Quitting Now");
+		Application.Quit();
+	}
+
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+	public float movementSpeed = 20f;
+	public float clockwise = -3.0f;
+	public float counterClockwise = 3.0f;
+	Rigidbody rb;
+
+	void Start () {
+		rb = this.GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		if(Input.GetKey(KeyCode.W)) {
+			rb.velocity = this.transform.forward * movementSpeed;
+		}
+		else if(Input.GetKey(KeyCode.S)) {
+			rb.velocity = -this.transform.forward * movementSpeed;
+		}
+		if(Input.GetKey(KeyCode.A)) {
+			this.transform.Rotate(0, clockwise, 0);
+		}
+		else if(Input.GetKey(KeyCode.D)) {
+			this.transform.Rotate(0, counterClockwise, 0);
+		}
+	}
+}

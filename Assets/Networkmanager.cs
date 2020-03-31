0646d009f1aa1316f1abf845d7824664fa850978
Tyler McPhee <32398415+tmcphee@users.noreply.github.com>
2020-03-30 22:13:41 -0400

    Implemented Multiplayer and menu.

---
+++ Assets/Networkmanager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Networkmanager : NetworkManager {
+
+	// Use this for initialization
+	int maxplayers = 2;
+	int currentplayers = 0;
+
+	/*public void StartGame(){
+		SceneManager.LoadScene("Multiplayer", LoadSceneMode.Single);
+	}*/
+
+	public void StartupHost(){
+		SetPort();
+		NetworkManager.singleton.StartHost();
+	}
+
+	public void JoinGame(){
+		if(currentplayers != maxplayers){
+			SetIPAddress();
+			SetPort();
+			NetworkManager.singleton.StartClient();
+			currentplayers++;
+		}
+	}
+
+	/*public void Disconnect(){
+		if (Network.isServer){
+			NetworkManager.singleton.StopHost();
+			NetworkManager.singleton.StopClient();
+		}
+		else{
+			NetworkManager.singleton.StopClient();
+		}
+	}*/
+
+	public void SetPort(){
+		string Port = GameObject.Find("PortFeild").transform.Find("Text").GetComponent<Text>().text;
+		Debug.Log("PORT SET: " + Port);
+		NetworkManager.singleton.networkPort = int.Parse(Port);
+	}
+
+	public void SetIPAddress(){
+		string IPAddress = GameObject.Find("IPAddressFeild").transform.Find("Text").GetComponent<Text>().text;
+		Debug.Log("IP SET: " + IPAddress);
+		NetworkManager.singleton.networkAddress = IPAddress;
+	}
+
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId){
+		GameObject player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+		NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+	}
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/Main_Menu.cs
+++ Assets/Scripts/Main_Menu.cs
@@ -4,9 +4,50 @@ using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Main_Menu : MonoBehaviour {
+	public GameObject ParentObject;
+
+	void Start(){
+		ParentObject = GameObject.Find("Canvas");
+		//ParentObject = this.gameObject.transform.parent.gameObject;
+
+		ParentObject.transform.GetChild(0).gameObject.SetActive(true);//Set Main Menu to disabled
+		ParentObject.transform.GetChild(1).gameObject.SetActive(false);//Set Options to disabled
+		ParentObject.transform.GetChild(2).gameObject.SetActive(false);//Set VehicleSelection to disabled
+		ParentObject.transform.GetChild(3).gameObject.SetActive(false);//Set MultiplayerMenu to disabled
+		ParentObject.transform.GetChild(4).gameObject.SetActive(false);//Set HostGameMenu to disabled
+		ParentObject.transform.GetChild(5).gameObject.SetActive(false);//Set JoinGameMenu to disabled
+	}
 
 	public void playGame(){
-		SceneManager.LoadScene("Multiplayer", LoadSceneMode.Single);
+		ParentObject.transform.GetChild(0).gameObject.SetActive(false);//Set Main Menu to disabled
+		ParentObject.transform.GetChild(3).gameObject.SetActive(true);//Set MultiplayerSetup to Enabled
+		//SceneManager.LoadScene("Multiplayer", LoadSceneMode.Single);
+	}
+
+	public void HostGame(){
+		ParentObject.transform.GetChild(3).gameObject.SetActive(false);//Set MultiplayerMenu to disabled
+		ParentObject.transform.GetChild(4).gameObject.SetActive(true);//Set HostGameMenu to Enabled
+	}
+	public void JoinGame(){
+		ParentObject.transform.GetChild(3).gameObject.SetActive(false);//Set MultiplayerMenu to disabled
+		ParentObject.transform.GetChild(5).gameObject.SetActive(true);//Set JoinGameMenu to Enabled
+	}
+
+	public void BackToMultiplayerMenu()
+	{
+		ParentObject.transform.GetChild(3).gameObject.SetActive(true);//Set MultiplayerMenu to Enabled
+		ParentObject.transform.GetChild(4).gameObject.SetActive(false);//Set HostGameMenu to Enabled
+		ParentObject.transform.GetChild(5).gameObject.SetActive(false);//Set JoinGameMenu to Enabled
+	}
+
+	public void BackToMainMenu()
+	{
+		ParentObject.transform.GetChild(0).gameObject.SetActive(true);//Set Main Menu to Enabled
+		ParentObject.transform.GetChild(1).gameObject.SetActive(false);//Set Options to disabled
+		ParentObject.transform.GetChild(2).gameObject.SetActive(false);//Set VehicleSelection to disabled
+		ParentObject.transform.GetChild(3).gameObject.SetActive(false);//Set MultiplayerMenu to disabled
+		ParentObject.transform.GetChild(4).gameObject.SetActive(false);//Set HostGameMenu to disabled
+		ParentObject.transform.GetChild(5).gameObject.SetActive(false);//Set JoinGameMenu to disabled
 	}
 
 	public void quitGame(){
